288434dee7e894b927a78058d9461d7a64d4d313
Shayan <shayanghodos@gmail.com>
2023-12-09 14:33:17 +0330

    change permisson

--- backend/market/permissions.py
+++ backend/market/permissions.py
@@ -14,27 +14,33 @@ class IsAdminOrArtist(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         else:
-            if request.user.id:
+            if request.user:
                 if request.user.is_staff:
                     return True
                 
-                customer = Customer.objects.get(user_id=request.user.id)
-                if customer.membership == "A" and request.method == "POST":
-                    return True
-                
-                elif request.method in ["PUT", "DELETE"]:
-                    art = ArtWork.objects.get(id=view.kwargs["pk"])
-                    if art.owner.user.id == request.user.id:
+                try:
+                    customer = Customer.objects.get(user_id=request.user.id)
+                    if customer.membership == "A" and request.method == "POST":
                         return True
+                    
+                    elif request.method in ["PUT", "DELETE"]:
+                        art = ArtWork.objects.get(id=view.kwargs["pk"])
+                        if art.owner.user.id == request.user.id:
+                            return True
+                except:
+                    return False
         return False
 
 
 class IsAdminOrCustomer(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.id:
+        if request.user:
             if request.user.is_staff:
                 return True
-            customer = Customer.objects.get(user_id=request.user.id)
-            if customer.id == int(view.kwargs["customer_pk"]):
-                return True
+            try:
+                customer = Customer.objects.get(user_id=request.user.id)
+                if customer.id == int(view.kwargs["customer_pk"]):
+                    return True
+            except:
+                return False
         return False
--- backend/market/tests/test_arts.py
+++ backend/market/tests/test_arts.py
@@ -10,3 +10,16 @@ class TestCreateArt:
         response = client.post('/market/arts/', {'name': 'a', 'descriptions': 'b'})
         
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    
+    
+    def test_if_user_is_not_admin_return_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/market/arts/', {'name': 'a', 'descriptions': 'b'})
+        
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    
+
+
+        
