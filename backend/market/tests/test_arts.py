3c228a1a6795e0b51e4ab8c66b269ee995ddd691
Shayan <shayanghodos@gmail.com>
2023-12-09 13:58:01 +0330

    git ignor

--- backend/market/tests/test_arts.py
+++ backend/market/tests/test_arts.py
@@ -1,7 +1,9 @@
 from rest_framework.test import APIClient
 from rest_framework import status
+import pytest
 
 
+@pytest.mark.django_db
 class TestCreateArt:
     def test_if_user_is_anonymous_return_401(self):
         client = APIClient()
