255e32af78ed0427dae9e281dd1ca9b9ca7c482a
Shayan <shayanghodos@gmail.com>
2023-12-09 13:32:58 +0330

    add tset

---
+++ backend/market/tests/test_arts.py
@@ -0,0 +1,10 @@
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+class TestCreateArt:
+    def test_if_user_is_anonymous_return_401(self):
+        client = APIClient()
+        response = client.post('/market/arts/', {'name': 'a', 'descriptions': 'b'})
+        
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
