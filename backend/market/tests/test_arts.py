7e435f4a54f9e4ece5adc41b7ddf49534e2cbacf
Shayan <shayanghodos@gmail.com>
2023-12-14 22:23:41 +0330

    test art

--- backend/market/tests/test_arts.py
+++ backend/market/tests/test_arts.py
@@ -17,12 +17,35 @@ def create_art(api_client):
 
 @pytest.fixture
 def get_art(api_client):
-    def do_get_art(art, user=False):
+    def do_get_art(id="", user=False):
         client = api_client
         if user != False:
             client.force_authenticate(user=user)
-        return client.get(f'/market/arts/{art.id}/')
+        if id:
+            id = str(id) + '/'
+        print(f'/market/arts/{id}')
+        return client.get(f'/market/arts/{id}')
     return do_get_art
+
+
+@pytest.fixture
+def put_art(api_client):
+    def do_put_art(id, art={}, user=False):
+        client = api_client
+        if user != False:
+            client.force_authenticate(user=user)
+        return client.put(f'/market/arts/{id}/', art)
+    return do_put_art
+
+
+@pytest.fixture
+def delete_art(api_client):
+    def do_delete_art(id, user=False):
+        client = api_client
+        if user != False:
+            client.force_authenticate(user=user)
+        return client.delete(f'/market/arts/{id}/')
+    return do_delete_art
 
 
 
@@ -30,6 +53,7 @@ def get_art(api_client):
 class TestCreateArt:
     def test_if_user_is_anonymous_return_401(self, create_art):
         response = create_art()
+        print(response.data)
         
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
@@ -42,7 +66,9 @@ class TestCreateArt:
 
     def test_if_data_is_invalid_return_400(self, create_attist_customer, create_art):
         user = create_attist_customer()["user"]
+        
         response = create_art(art={'name': '', 'descriptions': 'b'}, user=user)
+        print(response.data)
         
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['name'] is not None
@@ -50,9 +76,12 @@ class TestCreateArt:
 
     def test_if_data_is_valid_return_201(self, create_attist_customer, create_art):
         user = create_attist_customer()["user"]
+        
         response = create_art(user=user)
+        print(response.data)
         
         assert response.status_code == status.HTTP_201_CREATED
+
 
 
 @pytest.mark.django_db
@@ -60,17 +89,119 @@ class TestRetrieveArt:
     def test_if_art_exist_return_200(self, create_attist_customer, get_art):
         customer = create_attist_customer()["customer"]
         art = baker.make(ArtWork, artist=customer, owner=customer)
-        response = get_art(art)
-            
-        # object_list = ["id", "name", "descriptions", "artist_id"]
-        # response_list = ["id", "name", "descriptions", "artist"]
-        # check_dict = lambda x: x["id"] if type(x) == OrderedDict else x
         
-        # art_check = {item: check_dict(art.__dict__[item]) for item in object_list}
-        # response_check = {item: check_dict(response.data[item]) for item in response_list}
+        response = get_art(art.id)
+        print(response.data)
         
         assert response.status_code == status.HTTP_200_OK
-            
 
 
+@pytest.mark.django_db
+class TestListArt:
+    def test_if_arts_exist_return_200(self, create_attist_customer, get_art):
+        customer = create_attist_customer()["customer"]
+        baker.make(ArtWork, artist=customer, owner=customer, _quantity=10)
         
+        response = get_art()
+        print(response.data)
+        
+        assert response.status_code == status.HTTP_200_OK
+
+
+          
+@pytest.mark.django_db
+class TestChangeArt:
+    def test_if_user_is_anonymous_return_401(self, create_attist_customer, put_art):
+        customer = create_attist_customer()["customer"]
+        
+        art = baker.make(ArtWork, artist=customer, owner=customer)
+        art_2 = baker.make(ArtWork, artist=customer, owner=customer)
+        put_data = {item: art_2.__dict__[item] for item in art_2.__dict__ if item in ["name", "price", "show_art"]}
+
+        response = put_art(art.id, put_data)
+        print(response.data)
+        
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+    def test_if_user_is_not_admin_return_403(self, create_attist_customer, put_art):
+        customer = create_attist_customer()["customer"]
+        
+        art = baker.make(ArtWork, artist=customer, owner=customer)
+        art_2 = baker.make(ArtWork, artist=customer, owner=customer)
+        put_data = {item: art_2.__dict__[item] for item in art_2.__dict__ if item in ["name", "price", "show_art"]}
+
+        response = put_art(art.id, put_data, {})
+        print(response.data)
+        
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+    def test_if_data_is_invalid_return_400(self, create_attist_customer, put_art):
+        user_customer = create_attist_customer()
+        customer = user_customer["customer"]
+        user = user_customer["user"]
+        
+        art = baker.make(ArtWork, artist=customer, owner=customer)
+        art_2 = baker.make(ArtWork, artist=customer, owner=customer)
+        put_data = {item: art_2.__dict__[item] for item in art_2.__dict__ if item in ["name", "price", "show_art"]}
+         
+        response = put_art(art.id, put_data, user)
+        print(response.data)
+        
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+    def test_if_data_is_valid_return_201(self, create_attist_customer, put_art):
+        user_customer = create_attist_customer()
+        customer = user_customer["customer"]
+        user = user_customer["user"]
+        
+        art = baker.make(ArtWork, artist=customer, owner=customer)
+        art_2 = baker.make(ArtWork, artist=customer, owner=customer)
+        put_data = {item: art_2.__dict__[item] for item in art_2.__dict__ if item in ["name", "price", "show_art"]}
+        put_data["name"] = put_data["name"][:-10] + "abc"
+
+        response = put_art(art.id, put_data, user)
+        print(response.data)
+        
+        assert response.status_code == status.HTTP_200_OK
+
+
+      
+@pytest.mark.django_db
+class TestDeleteArt:
+    def test_if_user_is_anonymous_return_401(self, create_attist_customer, delete_art):
+        customer = create_attist_customer()["customer"]
+        
+        art = baker.make(ArtWork, artist=customer, owner=customer)
+
+        response = delete_art(art.id)
+        print(response.data)
+        
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+    def test_if_user_is_not_admin_return_403(self, create_attist_customer, delete_art):
+        customer = create_attist_customer()["customer"]
+        
+        art = baker.make(ArtWork, artist=customer, owner=customer)
+
+        response = delete_art(art.id, {})
+        print(response.data)
+        
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+    def test_if_data_is_valid_return_201(self, create_attist_customer, delete_art):
+        user_customer = create_attist_customer()
+        customer = user_customer["customer"]
+        user = user_customer["user"]
+        
+        art = baker.make(ArtWork, artist=customer, owner=customer)
+
+        response = delete_art(art.id, user)
+        print(response.data)
+        
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
