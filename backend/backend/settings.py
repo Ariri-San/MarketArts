60c237dcc311977aa4a51b75dfeab765840a8f8d
Shayan <shayanghodos@gmail.com>
2024-06-25 17:16:35 +0330

    debug

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w_kj&-0i3!wwx@2c5_+fv75m1f80^t&399hlyhn#h^%u$!v1-1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['marketarts-backend.onrender.com']
 
 
 # Application definition
